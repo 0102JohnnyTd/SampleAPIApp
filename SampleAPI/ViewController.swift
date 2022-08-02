b4bcac43b9cbd3b2c47aa0af6e78550c90fbf710
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-02 23:05:42 +0900

    Default commit

---
+++ SampleAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleAPI
+//
+//  Created by Johnny Toda on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
