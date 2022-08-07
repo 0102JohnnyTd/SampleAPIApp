9be1b82ec7e0cef6fa7b232cf59da0373b622821
0102johnny <rt2824.recruit.1022@gmail.com>
2022-08-07 17:52:48 +0900

    アプリ起動時に通信を行い、取得したJSONデータをcellに表示させる処理の実装

--- SampleAPI/ViewController.swift
+++ SampleAPI/ViewController.swift
@@ -11,16 +11,39 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var searchBar: UISearchBar!
     @IBOutlet private weak var pokemonListTableView: UITableView!
     @IBOutlet private weak var indicator: UIActivityIndicatorView!
+
+    private var dataArray: [Pokemon] = []
+    private var jaNameDataArray: [PokemonJaName] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTableViewCell()
+        fetchData()
+        fetchJaNameData()
     }
 
     private func setUpTableViewCell() {
         pokemonListTableView.delegate = self
         pokemonListTableView.dataSource = self
         pokemonListTableView.register(PokemonListTableViewCell.nib, forCellReuseIdentifier: PokemonListTableViewCell.identifier)
+    }
+
+    private func fetchData() {
+        FetchAPIs.decodePokemonData { [self] in
+            dataArray = $0
+            DispatchQueue.main.async { [self] in
+                self.pokemonListTableView.reloadData()
+            }
+        }
+    }
+
+    private func fetchJaNameData() {
+        FetchAPIs.decodePokemonNameData { [self] in
+            jaNameDataArray = $0
+            DispatchQueue.main.async { [self] in
+                pokemonListTableView.reloadData()
+            }
+        }
     }
 }
 
@@ -30,10 +53,14 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        493
+        jaNameDataArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        pokemonListTableView.dequeueReusableCell(withIdentifier: PokemonListTableViewCell.identifier, for: indexPath) as! PokemonListTableViewCell
+        let cell = pokemonListTableView.dequeueReusableCell(withIdentifier: PokemonListTableViewCell.identifier, for: indexPath) as! PokemonListTableViewCell
+
+        cell.configure(imageURL: URL(string: dataArray[indexPath.row].sprites.frontImage)!, id: "\(dataArray[indexPath.row].id)", enName: dataArray[indexPath.row].name, jaName: jaNameDataArray[indexPath.row].names[0].name)
+
+        return cell
     }
 }
